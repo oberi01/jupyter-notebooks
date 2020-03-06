d528aedfddbdeaee63eac3f81a8fcaa6f5e964c0
oberi01 <oberi01@me.com>
2020-03-06 20:10:21 +0100

    stacked_bar.py: initial version

---
+++ README.md
@@ -0,0 +1,12 @@
+### Purpose
+This project holds a selection of jupyter notebooks 
+that might be helpful when working with pandas
+
+### Why are there .py files
+Since ipynb files change with each execution of a notebook, 
+keeping them in git is not advisable. Fortunately the jupytext project 
+provides a .py representation of a notebook which can be kept in git.
+
+For an explanation how this works, see:
+
+https://towardsdatascience.com/introducing-jupytext-9234fdff6c57
---
+++ stacked_bar.py
@@ -0,0 +1,37 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+### Plot stacked bar data read from csv file
+
+# Render plots inline
+# %matplotlib inline
+from os.path import expandvars
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = (15, 5)
+
+# change dir to jupyter project folder
+import os
+os.chdir(expandvars('$HOME/gitwork/jupyter-notebooks'))
+# -
+
+df = pd.read_csv('./data/fnbp2.csv', encoding='latin1', sep=';', 
+                 parse_dates=['date'], index_col='date' ) # usecols=
+
+# Show the first rows
+df[:3]
+
+df.plot.bar(stacked=True, color=['green','blue','red','lightgrey']);
